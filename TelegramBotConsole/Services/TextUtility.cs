f804e36def47add1dc657493bb493d10cc29deb8
KYA12 <21127728+KYA12@users.noreply.github.com>
2019-10-20 17:17:33 +0300

    Add files via upload

---
+++ TelegramBotConsole/Services/TextUtility.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace TelegramBotConsole.Services
+{
+    public class TextUtility
+    {
+        public static string[] ConvertForTranslate(string textForTranslate)
+        {
+            string[] wordArrayForTranslate = textForTranslate.Split(new[] { ' ', '.', ',', '?', '!', ':', ';', '-', '\"', '/' },
+                             StringSplitOptions.RemoveEmptyEntries);// Создание массив слов из текста пользователя, по разделителям и удаляем лишние пробелы
+            return wordArrayForTranslate;
+        }
+        public static string[] ConvertOriginal(string textOriginal) 
+        {
+            string[] wordArrayOriginal = TextUtility.AddSpacesBeforePunctuation(textOriginal).Split(new[] { ' ' },
+                       StringSplitOptions.RemoveEmptyEntries);// Создание массива слов включаю разделители и удаляем пробелы
+            return wordArrayOriginal;
+
+        }
+
+        public static string AddSpacesBeforePunctuation(string stringOriginal)
+        {
+            var builder = new StringBuilder();
+            char? previousChar = null;
+
+            foreach (var ch in stringOriginal)
+            {
+                // Добавление пробела перед знаком пунктуации
+                if ((Char.IsPunctuation(ch) || ch =='-')&& previousChar != ' ' )
+                {
+                    builder.Append(' ');
+                }
+                if (previousChar =='-')
+                {
+                    builder.Append(' ');
+                }
+                builder.Append(ch);
+                previousChar = ch;
+            }
+            // Строка с пробелами между словами и знаками пунктуации
+            string stringChanged = builder.ToString();
+            return stringChanged;
+        }
+        public static StringBuilder RemoveSpacesBeforePunctuationAndAddWord(string[] wordArray)
+        {
+            StringBuilder result = new StringBuilder(); // Строка с результатом перевода слова/фразы
+            for (int i = 0; i < wordArray.Length; i++)
+            {
+                if (Char.IsPunctuation(wordArray[i].First()))
+                {
+                    result.Length--;
+                }
+                result.Append(wordArray[i]);
+                result.Append(" ");
+            }
+            return result;
+        }
+    }
+}
---
+++ TelegramBotConsole/Services/TranslateService.cs
@@ -0,0 +1,33 @@
+﻿using GoogleTranslateFreeApi;
+using System;
+using System.Threading.Tasks;
+
+namespace TelegramBotConsole.Services
+{
+    public class Translate
+    {
+        /* Метод переводит слова/фразы с одного языка на другой и возвращает перевод */
+        public static async Task<string> GoogleTranslate(string s, Serilog.ILogger logger, string fromLanguage, string toLanguage)
+        {
+            if (s.Length > 0)
+            {
+                var translator = new GoogleTranslator();
+                Language from = GoogleTranslator.GetLanguageByName(fromLanguage);
+                Language to = GoogleTranslator.GetLanguageByName(toLanguage);
+                try
+                {
+                    TranslationResult result = await translator.TranslateLiteAsync(s, from, to);
+                    return result.MergedTranslation;
+                }
+                catch (Exception ex)
+                {
+                    logger.Error($"Error is: {ex.Message}");
+                }
+                return null;
+            }
+            string str = "";
+            return str;
+        }
+    }
+}
+
